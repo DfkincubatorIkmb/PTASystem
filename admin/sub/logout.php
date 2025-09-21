b6a40cbcc3aaa4d7d01f01ad4a8bcfa435967ed9
Ahmad Zahid <128762005+Ahmadzahid25@users.noreply.github.com>
2025-09-21 15:10:46 +0800

    Delete admin directory

--- admin/sub/logout.php
+++
@@ -1,9 +0,0 @@
-<?php
-session_start();
-session_unset();
-session_destroy();
-$_SESSION['errmsg']="You have successfully logout";
-?>
-<script language="javascript">
-document.location="../index.php";
-</script>
